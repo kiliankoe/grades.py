f0c3172c87f89535f2fddd9606faa081a47a3957
Kilian Koeltzsch <me@kilian.io>
2016-12-27 20:46:49 +0100

    fix persistence bug with .known_grades

--- persistence.py
+++ persistence.py
@@ -1,12 +1,18 @@
 # "Persistence"
 
+from os import path
+
 def filter_new(grades):
-  with open('.known_grades', 'w+') as known_file:
+  if not path.isfile('.known_grades'):
+    open('.known_grades', 'a').close()
+
+  with open('.known_grades', 'r+') as known_file:
     known_grades = known_file.read().splitlines()
     new_grades = []
     for grade in grades:
       if grade['PrNr'] not in known_grades:
         new_grades.append(grade)
         known_file.write(grade['PrNr'])
+        known_file.write('\n')
 
     return new_grades
